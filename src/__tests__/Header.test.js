22293b3967d6c49c0b896bb27935092507faa5bd
Koome <judekoome360@gmail.com>
2023-04-17 20:28:57 +0300

    Wierd

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,15 @@ import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
-
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  expect(
+    screen.getByText((content, element) => {
+      const hasText = (node) => node.textContent === "hello from the Header!";
+      const nodeHasText =
+        hasText(element) ||
+        (element.querySelector("*") &&
+          Array.from(element.childNodes).some(hasText));
+      return nodeHasText;
+    })
+  ).toBeInTheDocument();
 });
+``
